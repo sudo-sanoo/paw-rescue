122ae370d8cc669757c711a5aef9ef9bc62a1b45
sudo-sanoo <194980529+sudo-sanoo@users.noreply.github.com>
2025-12-08 01:49:19 +0800

    update: title change

--- app/index.php
+++ app/index.php
@@ -8,7 +8,7 @@ require_once "includes/db.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PawRescue - Saving Lives Together</title>
+    <title>PawRescue</title>
     
     <!-- CSS -->
     <link rel="stylesheet" href="css/index.css">
